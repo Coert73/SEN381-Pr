7c80b9d42bb5205f8bbfb0f16fe7dcd1eb07e4cc
GlitchBase <576357@student.belgiumcampus.ac.za>
2021-03-19 12:31:49 +0200

    Added content to client, business and individual

--- SEN381 Pr/BusinessClient.cs
+++ SEN381 Pr/BusinessClient.cs
@@ -5,8 +5,27 @@ using System.Text;
 
 namespace SEN381_Pr
 {
-    public class BusinessClient
+    public class BusinessClient : Client
     {
-      
+        public override string BusinessID { get => BusinessID; set => BusinessID = value; }
+        public override Contract ClientContract { get => ClientContract; set => ClientContract = value; }
+        public override Address ClientAddress { get => ClientAddress; set => ClientAddress = value; }
+        public override string ClientName { get => ClientName; set => ClientName = value; }
+        public override string ClientSurname { get => ClientSurname; set => ClientSurname = value; }
+        public override string ClientNumber { get => ClientNumber; set => ClientNumber = value; }
+        public override string CallsMade { get => CallsMade; set => CallsMade = value; }
+        public override string Position { get => Position; set => Position = value; }
+
+        public BusinessClient(string businessID, Contract clientContract, Address clientAddress, string clientName, string clientSurname, string clientNumber, string callsMade, string position)
+        {
+            BusinessID = businessID;
+            ClientContract = clientContract;
+            ClientAddress = clientAddress;
+            ClientName = clientName;
+            ClientSurname = clientSurname;
+            ClientNumber = clientNumber;
+            CallsMade = callsMade;
+            Position = position;
+        }
     }
 }

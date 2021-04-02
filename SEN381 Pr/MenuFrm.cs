5b3cf1a38546001e0c1d7e45dde17236b60939f3
GlitchBase <576357@student.belgiumcampus.ac.za>
2021-04-02 15:01:12 +0200

    Added Juan Forms Base

---
+++ SEN381 Pr/MenuFrm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEN381_Pr
+{
+    public partial class MenuFrm : Form
+    {
+        OptionsFrm Options = new OptionsFrm();
+        public MenuFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCallOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReportsOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRequestsOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnContractsOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClientsOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnJobsOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTechOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOptionsOpen_Click(object sender, EventArgs e)
+        {
+            Options.Show();
+            this.Hide();
+        }
+
+        private void btnLogOut_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SEN381 Pr/OptionsFrm.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace SEN381_Pr
+{
+    partial class OptionsFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OptionsFrm));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnVisOptions = new System.Windows.Forms.Button();
+            this.btnAudioOptions = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(8, 68);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(369, 42);
+            this.panel2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(136, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Options";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(8, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(369, 60);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(340, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Premiere Service Solutions";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.btnAudioOptions);
+            this.panel3.Controls.Add(this.btnVisOptions);
+            this.panel3.Location = new System.Drawing.Point(8, 116);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(367, 193);
+            this.panel3.TabIndex = 4;
+            // 
+            // btnVisOptions
+            // 
+            this.btnVisOptions.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVisOptions.Location = new System.Drawing.Point(15, 16);
+            this.btnVisOptions.Name = "btnVisOptions";
+            this.btnVisOptions.Size = new System.Drawing.Size(342, 33);
+            this.btnVisOptions.TabIndex = 3;
+            this.btnVisOptions.Text = "Visual Options";
+            this.btnVisOptions.UseVisualStyleBackColor = true;
+            // 
+            // btnAudioOptions
+            // 
+            this.btnAudioOptions.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAudioOptions.Location = new System.Drawing.Point(15, 55);
+            this.btnAudioOptions.Name = "btnAudioOptions";
+            this.btnAudioOptions.Size = new System.Drawing.Size(342, 33);
+            this.btnAudioOptions.TabIndex = 4;
+            this.btnAudioOptions.Text = "Audio Options";
+            this.btnAudioOptions.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(342, 33);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Return";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // OptionsFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 321);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "OptionsFrm";
+            this.Text = "Options";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnVisOptions;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnAudioOptions;
+    }
+}

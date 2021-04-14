089566a38d875c7ebec7440415e0fdab4f6f77d4
Willem Botha <sniperkeep14@gmail.com>
2021-04-14 12:08:56 +0200

    Added My Froms

---
+++ SEN381 Pr/Form2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEN381_Pr
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}

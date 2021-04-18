74f2f7a14d1d1c794dbcf41309ba140db57a5b07
Deann10 <70572144+Deann10@users.noreply.github.com>
2021-04-18 18:08:46 +0200

    My forms

---
+++ SEN381 Pr/Presentation Layer/ContractsFrm.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace SEN381_Pr.Presentation_Layer
+{
+    partial class ContractsFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ContractsFrm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgvContracts = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnContract = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtContractID = new System.Windows.Forms.TextBox();
+            this.lblContractID = new System.Windows.Forms.Label();
+            this.lblContractType = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.lblContractName = new System.Windows.Forms.Label();
+            this.txtContractType = new System.Windows.Forms.TextBox();
+            this.txtContractName = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContracts)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.dgvContracts);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(479, 485);
+            this.panel1.TabIndex = 1;
+            // 
+            // dgvContracts
+            // 
+            this.dgvContracts.BackgroundColor = System.Drawing.Color.White;
+            this.dgvContracts.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvContracts.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dgvContracts.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvContracts.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvContracts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContracts.GridColor = System.Drawing.Color.MediumSpringGreen;
+            this.dgvContracts.Location = new System.Drawing.Point(3, 3);
+            this.dgvContracts.Name = "dgvContracts";
+            this.dgvContracts.RowHeadersVisible = false;
+            this.dgvContracts.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvContracts.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvContracts.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvContracts.Size = new System.Drawing.Size(473, 456);
+            this.dgvContracts.TabIndex = 0;
+            this.dgvContracts.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTechnician_CellContentClick);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.btnDelete);
+            this.panel3.Controls.Add(this.btnEdit);
+            this.panel3.Controls.Add(this.btnContract);
+            this.panel3.Location = new System.Drawing.Point(497, 269);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(381, 178);
+            this.panel3.TabIndex = 4;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.FlatAppearance.BorderColor = System.Drawing.Color.MediumSpringGreen;
+            this.btnDelete.FlatAppearance.BorderSize = 3;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Impact", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(15, 118);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(351, 36);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.FlatAppearance.BorderColor = System.Drawing.Color.MediumSpringGreen;
+            this.btnEdit.FlatAppearance.BorderSize = 3;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEdit.Font = new System.Drawing.Font("Impact", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(15, 65);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(351, 36);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnContract
+            // 
+            this.btnContract.FlatAppearance.BorderColor = System.Drawing.Color.MediumSpringGreen;
+            this.btnContract.FlatAppearance.BorderSize = 3;
+            this.btnContract.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnContract.Font = new System.Drawing.Font("Impact", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContract.Location = new System.Drawing.Point(15, 14);
+            this.btnContract.Name = "btnContract";
+            this.btnContract.Size = new System.Drawing.Size(351, 36);
+            this.btnContract.TabIndex = 0;
+            this.btnContract.Text = "Create Contract";
+            this.btnContract.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.txtContractID);
+            this.panel2.Controls.Add(this.lblContractID);
+            this.panel2.Controls.Add(this.lblContractType);
+            this.panel2.Controls.Add(this.textBox8);
+            this.panel2.Controls.Add(this.textBox7);
+            this.panel2.Controls.Add(this.lblContractName);
+            this.panel2.Controls.Add(this.txtContractType);
+            this.panel2.Controls.Add(this.txtContractName);
+            this.panel2.Location = new System.Drawing.Point(497, 15);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(381, 242);
+            this.panel2.TabIndex = 3;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(14, 208);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(351, 3);
+            this.textBox1.TabIndex = 22;
+            // 
+            // txtContractID
+            // 
+            this.txtContractID.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtContractID.Location = new System.Drawing.Point(14, 191);
+            this.txtContractID.Multiline = true;
+            this.txtContractID.Name = "txtContractID";
+            this.txtContractID.Size = new System.Drawing.Size(351, 20);
+            this.txtContractID.TabIndex = 21;
+            // 
+            // lblContractID
+            // 
+            this.lblContractID.AutoSize = true;
+            this.lblContractID.Font = new System.Drawing.Font("Impact", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContractID.Location = new System.Drawing.Point(11, 160);
+            this.lblContractID.Name = "lblContractID";
+            this.lblContractID.Size = new System.Drawing.Size(68, 17);
+            this.lblContractID.TabIndex = 20;
+            this.lblContractID.Text = "Contract ID";
+            // 
+            // lblContractType
+            // 
+            this.lblContractType.AutoSize = true;
+            this.lblContractType.Font = new System.Drawing.Font("Impact", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContractType.Location = new System.Drawing.Point(11, 96);
+            this.lblContractType.Name = "lblContractType";
+            this.lblContractType.Size = new System.Drawing.Size(83, 17);
+            this.lblContractType.TabIndex = 19;
+            this.lblContractType.Text = "Contract Type";
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox8.Location = new System.Drawing.Point(14, 133);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(351, 3);
+            this.textBox8.TabIndex = 17;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox7.Location = new System.Drawing.Point(14, 60);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(351, 3);
+            this.textBox7.TabIndex = 16;
+            // 
+            // lblContractName
+            // 
+            this.lblContractName.AutoSize = true;
+            this.lblContractName.Font = new System.Drawing.Font("Impact", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContractName.Location = new System.Drawing.Point(11, 23);
+            this.lblContractName.Name = "lblContractName";
+            this.lblContractName.Size = new System.Drawing.Size(88, 17);
+            this.lblContractName.TabIndex = 6;
+            this.lblContractName.Text = "Contract Name";
+            // 
+            // txtContractType
+            // 
+            this.txtContractType.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtContractType.Location = new System.Drawing.Point(14, 116);
+            this.txtContractType.Multiline = true;
+            this.txtContractType.Name = "txtContractType";
+            this.txtContractType.Size = new System.Drawing.Size(351, 20);
+            this.txtContractType.TabIndex = 1;
+            // 
+            // txtContractName
+            // 
+            this.txtContractName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtContractName.Location = new System.Drawing.Point(14, 43);
+            this.txtContractName.Multiline = true;
+            this.txtContractName.Name = "txtContractName";
+            this.txtContractName.Size = new System.Drawing.Size(351, 20);
+            this.txtContractName.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(706, 456);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.FlatAppearance.BorderSize = 0;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(706, 456);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(172, 41);
+            this.btnBack.TabIndex = 11;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // ContractsFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(890, 497);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "ContractsFrm";
+            this.Text = "ContractsFrm";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContracts)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgvContracts;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnContract;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblContractID;
+        private System.Windows.Forms.Label lblContractType;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label lblContractName;
+        private System.Windows.Forms.TextBox txtContractType;
+        private System.Windows.Forms.TextBox txtContractName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtContractID;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnBack;
+    }
+}

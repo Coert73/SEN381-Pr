3ceea738bda57511593ba33f6b1babad4d8e9fd0
GlitchBase <576357@student.belgiumcampus.ac.za>
2021-04-22 15:07:47 +0200

    Fixed client form, developed client controller and fixed client class

--- SEN381 Pr/Data Access Layer/ClientADOController.cs
+++ SEN381 Pr/Data Access Layer/ClientADOController.cs
@@ -15,19 +15,19 @@ namespace SEN381_Pr
             return Controller.CarryCommand("SELECT * FROM Clients");
         }
 
-        public DataSet InsertClient(int busid,int conid,string name, string surname, string number, string callsmade, string pos)
+        public DataSet InsertClient(IndividualClient client)
         {
-            return Controller.CarryCommand($"INSERT INTO Clients (BusinessId,ContractId,Name,Surname,Number,CallsMade,Position) VALUES ({busid},{conid},'{name}','{surname}','{number}','{callsmade}','{pos}')");
+            return Controller.CarryCommand($"INSERT INTO Clients (BusinessId,ContractId,Name,Surname,Number,CallsMade,Position,AlternativeContact,AlternativeNumber,ClientId) VALUES ({client.BusinessID},{client.ClientContract},'{client.ClientName}','{client.ClientSurname}','{client.ClientNumber}','{client.CallsMade}','{client.Position}','{client.AltContact}','{client.AltNum}','{client.ClientID}')");
         }
 
-        public DataSet DeleteClient(int id)
+        public DataSet DeleteClient(IndividualClient client)
         {
-            return Controller.CarryCommand($"DELETE FROM Clients WHERE Client = {id}");
+            return Controller.CarryCommand($"DELETE FROM Clients WHERE Client = '{client.ClientID}'");
         }
 
-        public DataSet UpdateClient(int id)
+        public DataSet UpdateClient(IndividualClient client)
         {
-            return Controller.CarryCommand($"UPDATE Clients SET  WHERE = {id}");
+            return Controller.CarryCommand($"UPDATE Clients SET BusinessId='{client.BusinessID}',ContractId='{client.ClientContract}',Name='{client.ClientName}',Surname='{client.ClientSurname}',Number='{client.ClientNumber}',CallsMade='{client.CallsMade}',Position='{client.Position}',AlternativeContact='{client.AltContact}',AlternativeNumber='{client.AltNum}'  WHERE ClientId = '{client.ClientID}'");
         }
 
     }
